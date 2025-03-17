47f35b05745165eff6ad252c3551b44df0355148
Vojta Biberle <vojtech.biberle@gmail.com>
2025-03-17 23:01:17 +0100

    Modify tests-e2e.ts for Keboola
    Signed-off-by: Vojta Biberle <vojtech.biberle@gmail.com>

--- scripts/test-e2e.ts
+++ scripts/test-e2e.ts
@@ -6,18 +6,18 @@ import { readFileSync } from 'fs';
 dotenv.config();
 
 // Load the PR ID
-const owner = 'demandio';
-const repo = process.argv[2] || 'simplycodes-extension';
-const prId = process.argv[3] || 982;
-const projectContext = process.argv[4] || 'This is a browser extension for SimplyCodes';
+const owner = process.argv[2] || 'keboola';
+const repo = process.argv[3] || 'connection';
+const pr_number = parseInt(process.argv[4], 10) || 982;
+const projectContext = process.argv[5] || 'This is a PHP project written in Symfony and Zend.';
 
 // Read the PR payload
 const prPayload = JSON.parse(
-  readFileSync(resolve(__dirname, `./pull-requests/test-pr-payload-${prId}.json`), 'utf8')
+  readFileSync(resolve(__dirname, `./pull-requests/test-pr-payload-${pr_number}.json`), 'utf8')
 );
 
 // Set required environment variables for the action
-process.env.GITHUB_EVENT_PATH = resolve(__dirname, `./pull-requests/test-pr-payload-${prId}.json`);
+process.env.GITHUB_EVENT_PATH = resolve(__dirname, `./pull-requests/test-pr-payload-${pr_number}.json`);
 process.env.GITHUB_WORKSPACE = resolve(__dirname, '..');
 process.env.GITHUB_REPOSITORY = `${owner}/${repo}`;
 process.env.GITHUB_CONTEXT = JSON.stringify({
@@ -35,7 +35,7 @@ process.env.INPUT_AI_API_KEY = process.env.INPUT_AI_API_KEY || process.env.OPENA
 process.env.INPUT_AI_MODEL = process.env.INPUT_AI_MODEL || 'gpt-4o-mini';
 // process.env.INPUT_REVIEW_MAX_COMMENTS = process.env.INPUT_REVIEW_MAX_COMMENTS || '10';
 process.env.INPUT_EXCLUDE_PATTERNS = '**/*.lock,**/*.json,**/*.md';
-process.env.INPUT_APPROVE_REVIEWS = 'true';
+process.env.INPUT_APPROVE_REVIEWS = 'false';
 process.env.INPUT_REVIEW_PROJECT_CONTEXT = projectContext;
 
 // Run the action
